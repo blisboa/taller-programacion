d84a31afef6ccd10cf7c5fe9f3476d9eea2be1ec
bernardo.lisboa <bernardo.lisboa@sonda.com>
2020-10-02 23:41:38 -0300

    nuevas operaciones
    se agregan operacion de resta, multi y division

--- calculadora-v1/calculadora.Designer.cs
+++ calculadora-v1/calculadora.Designer.cs
@@ -36,82 +36,136 @@
             this.lblResultado = new System.Windows.Forms.Label();
             this.btnCalcular = new System.Windows.Forms.Button();
             this.lblInstrucciones = new System.Windows.Forms.Label();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtNumero1
             // 
-            this.txtNumero1.Location = new System.Drawing.Point(133, 50);
+            this.txtNumero1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero1.Location = new System.Drawing.Point(44, 116);
             this.txtNumero1.Name = "txtNumero1";
-            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.Size = new System.Drawing.Size(108, 35);
             this.txtNumero1.TabIndex = 0;
             // 
             // txtNumero2
             // 
-            this.txtNumero2.Location = new System.Drawing.Point(133, 90);
+            this.txtNumero2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero2.Location = new System.Drawing.Point(158, 116);
             this.txtNumero2.Name = "txtNumero2";
-            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.Size = new System.Drawing.Size(108, 35);
             this.txtNumero2.TabIndex = 1;
             // 
             // txtResultado
             // 
-            this.txtResultado.Location = new System.Drawing.Point(133, 129);
+            this.txtResultado.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(44, 188);
             this.txtResultado.Name = "txtResultado";
-            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.Size = new System.Drawing.Size(222, 35);
             this.txtResultado.TabIndex = 2;
             // 
             // lblNumero1
             // 
             this.lblNumero1.AutoSize = true;
-            this.lblNumero1.Location = new System.Drawing.Point(32, 53);
+            this.lblNumero1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lblNumero1.Location = new System.Drawing.Point(46, 92);
             this.lblNumero1.Name = "lblNumero1";
-            this.lblNumero1.Size = new System.Drawing.Size(56, 13);
+            this.lblNumero1.Size = new System.Drawing.Size(84, 21);
             this.lblNumero1.TabIndex = 3;
             this.lblNumero1.Text = "Numero 1:";
             // 
             // lblNumero2
             // 
             this.lblNumero2.AutoSize = true;
-            this.lblNumero2.Location = new System.Drawing.Point(32, 96);
+            this.lblNumero2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lblNumero2.Location = new System.Drawing.Point(161, 92);
             this.lblNumero2.Name = "lblNumero2";
-            this.lblNumero2.Size = new System.Drawing.Size(56, 13);
+            this.lblNumero2.Size = new System.Drawing.Size(84, 21);
             this.lblNumero2.TabIndex = 4;
             this.lblNumero2.Text = "Numero 2:";
             // 
             // lblResultado
             // 
             this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(35, 135);
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(40, 164);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.Size = new System.Drawing.Size(79, 21);
             this.lblResultado.TabIndex = 5;
             this.lblResultado.Text = "Resultado";
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(133, 180);
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(286, 116);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(0);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcular.Size = new System.Drawing.Size(50, 53);
             this.btnCalcular.TabIndex = 6;
-            this.btnCalcular.Text = "Sumar";
+            this.btnCalcular.Text = "+";
             this.btnCalcular.UseVisualStyleBackColor = true;
             this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             this.btnCalcular.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnCalcular_MouseMove);
             // 
             // lblInstrucciones
             // 
-            this.lblInstrucciones.AutoSize = true;
-            this.lblInstrucciones.Location = new System.Drawing.Point(32, 9);
+            this.lblInstrucciones.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstrucciones.Location = new System.Drawing.Point(30, 25);
             this.lblInstrucciones.Name = "lblInstrucciones";
-            this.lblInstrucciones.Size = new System.Drawing.Size(392, 13);
+            this.lblInstrucciones.Size = new System.Drawing.Size(319, 49);
             this.lblInstrucciones.TabIndex = 7;
-            this.lblInstrucciones.Text = "Ingreso dos numeros , luego haga clic en el boton sumar y se desplegara la suma.\r" +
-    "\n";
+            this.lblInstrucciones.Text = "Ingreso dos numeros , luego haga clic en la operación que desea realizar";
+            this.lblInstrucciones.Click += new System.EventHandler(this.lblInstrucciones_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestar.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestar.Location = new System.Drawing.Point(346, 117);
+            this.btnRestar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(50, 52);
+            this.btnRestar.TabIndex = 8;
+            this.btnRestar.Text = "-";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(286, 179);
+            this.btnMultiplicar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(50, 47);
+            this.btnMultiplicar.TabIndex = 9;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDividir.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(346, 176);
+            this.btnDividir.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(50, 47);
+            this.btnDividir.TabIndex = 10;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.button2_Click);
             // 
             // calculadora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 305);
+            this.ClientSize = new System.Drawing.Size(506, 302);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnRestar);
             this.Controls.Add(this.lblInstrucciones);
             this.Controls.Add(this.btnCalcular);
             this.Controls.Add(this.lblResultado);
@@ -138,5 +192,8 @@
         private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Label lblInstrucciones;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
     }
 }
--- calculadora-v1/calculadora.cs
+++ calculadora-v1/calculadora.cs
@@ -44,5 +44,64 @@ namespace calculadora03
         {
             
         }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            // se definen 3 variables de tipo entero
+            int Numero1, Numero2, Resultado;
+
+            // ahora vamos a LEER los dos numeros ingresado por pantalla
+            Numero1 = int.Parse(txtNumero1.Text);
+            Numero2 = int.Parse(txtNumero2.Text);
+
+            // aqui estamos sumando 2 numeros enteros 
+            // y lo asignamos a la variable Resultado que tambien es un entero
+            Resultado = Numero1 - Numero2;
+
+            // aqui vamos a mostrar la suma en la caja de texto txtResultado
+            // El metodo ToString() nos permite convertir a string el contenido de uan variable diferente a strin (numeros)
+            txtResultado.Text = Resultado.ToString();
+        }
+
+        private void lblInstrucciones_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // se definen 3 variables de tipo entero
+            int Numero1, Numero2, Resultado;
+
+            // ahora vamos a LEER los dos numeros ingresado por pantalla
+            Numero1 = int.Parse(txtNumero1.Text);
+            Numero2 = int.Parse(txtNumero2.Text);
+
+            // aqui estamos sumando 2 numeros enteros 
+            // y lo asignamos a la variable Resultado que tambien es un entero
+            Resultado = Numero1 * Numero2;
+
+            // aqui vamos a mostrar la suma en la caja de texto txtResultado
+            // El metodo ToString() nos permite convertir a string el contenido de uan variable diferente a strin (numeros)
+            txtResultado.Text = Resultado.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // se definen 3 variables de tipo entero
+            int Numero1, Numero2, Resultado;
+
+            // ahora vamos a LEER los dos numeros ingresado por pantalla
+            Numero1 = int.Parse(txtNumero1.Text);
+            Numero2 = int.Parse(txtNumero2.Text);
+
+            // aqui estamos sumando 2 numeros enteros 
+            // y lo asignamos a la variable Resultado que tambien es un entero
+            Resultado = Numero1 / Numero2;
+
+            // aqui vamos a mostrar la suma en la caja de texto txtResultado
+            // El metodo ToString() nos permite convertir a string el contenido de uan variable diferente a strin (numeros)
+            txtResultado.Text = Resultado.ToString();
+        }
     }
 }
