93d6371443ca4ddbd431c37eaf90997175a295e4
bernardo.lisboa <bernardo.lisboa@sonda.com>
2020-10-28 15:13:11 -0300

    arreglos-01

---
+++ arreglos-01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglos_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota1, nota2, nota3;
+            nota1 = 7;
+
+            int[] notas; // definir un arreglo de enteros
+            notas = new int[3]; // creando un arreglo de 3 elementos, dimension 3, o de largo 3
+
+            notas[0] = 4; // asignando al primer elmento, primera celda el numero  4
+            notas[1] = 5; // asignando al segundo elmento, segunda celda el numero  5
+            notas[2] = 6; // asignando al tercer elmento, tercera celda el numero  6
+
+            //Console.WriteLine(notas[0].ToString());
+            //Console.WriteLine(notas[1].ToString());
+            //Console.WriteLine(notas[2].ToString());
+            Console.WriteLine("Iniciliazra en uno.");
+            int contador = 1;
+            while (contador <=3) 
+            // mientras el valor de la variable "contador" sea menor o igual a 3 haga lo siguient
+            {
+                Console.WriteLine(notas[contador-1].ToString());
+                contador = contador + 1;
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("Iniciliazra en cero.");
+            contador = 0;
+            while (contador <= 2)
+            // mientras el valor de la variable "contador" sea menor o igual a 3 haga lo siguient
+            {
+                Console.WriteLine(notas[contador].ToString());
+                contador = contador + 1;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ consola01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace consola01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			// mostrar por panatlla los priemros 10 numeros ( 1 al 10) en lineas separadas
+			int contador1 = 1;
+			int contador2 = 1;
+			int contador3 = 1;
+			while (contador1 <= 9)
+			{
+				Console.Write("=");
+				contador1 = contador1 + 1;
+			}
+			Console.WriteLine("=");
+			while (contador2 <= 5)
+			{
+				Console.Write("|");
+				contador2 = contador2 + 1;
+			}
+			while (contador3 <= 9)
+			{
+				Console.Write("=");
+				contador3 = contador3 + 1;
+			}
+			Console.WriteLine("=");
+		
+		Console.ReadKey();
+		}
+    }
+}
