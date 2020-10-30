7ef82fc3411f0fccb38758d47a22108fcf3d00cc
bernardo.lisboa <bernardo.lisboa@sonda.com>
2020-10-30 14:20:43 -0300

    rectangulo
    app de consola que dibuja un rectangulo con caracteres

---
+++ rectangulo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string horizontal = "X";
+            string vertical = "!";
+            string relleno = ".";
+
+            // imprimir el techo del rectangulo
+            int contador = 1;
+            while (contador <= 10)
+            {
+                Console.Write(horizontal);
+                contador++; // es lo mismo que contador = contador + 1
+            }
+            Console.WriteLine("");// Salto de linea
+
+            int contadorLineas = 1;
+            while (contadorLineas <= 5)
+            {
+                // imprimir una sola linea
+                Console.Write(vertical);
+                contador = 1;
+                while (contador <= 8)
+                {
+                    Console.Write(relleno);
+                    contador = contador + 1;
+                }
+                Console.WriteLine(vertical);
+
+                contadorLineas = contadorLineas + 1;
+            }
+
+            // imprimir el piso del rectangulo
+            contador = 1;
+            while (contador <= 10)
+            {
+                Console.Write(horizontal);
+                contador++; // es lo mismo que contador = contador + 1
+            }
+            Console.WriteLine("");// Salto de linea
+            Console.ReadKey(); // para evitar que se cierre la consola automaticamente
+
+        }
+    }
+}
